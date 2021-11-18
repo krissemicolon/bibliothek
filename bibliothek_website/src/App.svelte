1a3459b03fbd49afc80c45a0a9be3796f97d2420
kris huber [LU15561] <kris.huber@lukb.ch>
2021-11-18 09:07:17 +0100

    website

---
+++ bibliothek_website/src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Router from './routes/index.svelte'
+</script>
+
+<main>
+	<Router/>
+</main>
---
+++ bibliothek_website/src/main.js
@@ -0,0 +1,13 @@
+import App from './App.svelte';
+
+// Lightweight CSS Library
+import purecss from 'purecss';
+
+// Better API fetching
+import axios from "axios";
+
+var app = new App({
+	target: document.body
+});
+
+export default app;
